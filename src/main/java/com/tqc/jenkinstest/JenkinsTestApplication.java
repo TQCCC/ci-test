7d5e30697515bf1439bdd7325b7def193a49bd23
TQCCC <tqcccc@sina.com>
2017-07-18 16:44:01 +0800

    initial commit

---
+++ src/main/java/com/tqc/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,12 @@
+package com.tqc.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+}
