00729f8473d7944184d21b97e1cf8354dbed4ebd
TQCCC <tqcccc@sina.com>
2017-07-20 16:02:33 +0800

    -

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by win10 on 2017/7/20.
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "I'm jenkins test!";
+    }
+}
