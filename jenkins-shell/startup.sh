d46060d54e785cefee0bfcd1a371fd8f01c32d69
TQCCC <tqcccc@sina.com>
2017-10-26 10:23:08 +0800

    更新输出

--- jenkins-shell/startup.sh
+++ jenkins-shell/startup.sh
@@ -13,6 +13,6 @@ chmod 777 ${JAVA_APP_DIR}/${JAR_NAME}
 echo "执行jar包: " ${JAVA_APP_DIR}/${JAR_NAME}
 BUILD_ID=dontKillMe
 nohup java -jar ${JAVA_APP_DIR}/${JAR_NAME} &
-echo "**********************${JAR_NAME} started*************************"
+echo "${JAR_NAME} 已经启动"
 
 # TODO: 判断端口是否被占用，lsof -i:xxx
