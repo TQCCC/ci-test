457bf9d2918a9dfb4303be3f0f89beaa467085a7
qingchang.tang <qingchang96@sina.com>
2020-06-17 13:33:55 +0800

    mappings

--- src/main/java/io/tqc/jenkinstest/controller/BaseController.java
+++ src/main/java/io/tqc/jenkinstest/controller/BaseController.java
@@ -1,5 +1,6 @@
 package io.tqc.jenkinstest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class BaseController {
 
+    @GetMapping("/index")
     public String index() {
         return "JenkinsTest";
     }
