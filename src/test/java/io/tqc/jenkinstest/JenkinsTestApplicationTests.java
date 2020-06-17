487a71b837c3350a5a67bf9cd3ab8e7cf3b96b60
qingchang.tang <qingchang96@sina.com>
2020-06-17 13:42:08 +0800

    ..

--- src/test/java/io/tqc/jenkinstest/JenkinsTestApplicationTests.java
+++ src/test/java/io/tqc/jenkinstest/JenkinsTestApplicationTests.java
@@ -5,12 +5,29 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JenkinsTestApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+
+        System.out.println("===========Run Tests==============");
+        System.out.println("===========Run Tests==============");
+        System.out.println(LocalDateTime.now());
+        System.out.println("===========Run Tests==============");
+        System.out.println("===========Run Tests==============");
+        System.out.println("===========Run Tests==============");
+        System.out.println("===========Run Tests==============");
+        System.out.println("===========Run Tests==============");
+        System.out.println("===========Run Tests==============");
+        System.out.println("===========Run Tests==============");
+        System.out.println(LocalDateTime.now());
+        System.out.println("===========Run Tests==============");
+        System.out.println("===========Run Tests==============");
+
+    }
 
 }
